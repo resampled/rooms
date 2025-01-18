83ae27ef17de9a0d07c6518da0abcea3a4b5d479
resampled <133955578+resampled@users.noreply.github.com>
2025-01-18 19:23:54 +1030

    create basic models

--- webapp/models.py
+++ webapp/models.py
@@ -7,3 +7,18 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.username
+
+class Room(models.Model):
+    id = models.SlugField(unique=True, blank=False, null=True, max_length=50)
+    owner_nk = models.CharField(unique=False,null=True,max_length=200)
+    public_list = models.BooleanField()
+    passworded = models.BooleanField()
+    password = models.CharField(unique=False,null=True,max_length=200)
+    def __str__(self):
+        return self.id
+
+class Message(models.Model):
+    author_nk = models.CharField(unique=False,null=True,max_length=200)
+    author_ip = models.CharField(unique=False,null=True,max_length=80)
+    message = models.TextField(unique=False,null=True,max_length=1400)
+    room = models.ForeignKey('Room',on_delete=models.CASCADE, null=True)
