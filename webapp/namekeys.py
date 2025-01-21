ed48983db0003dcf8b566e300aba0b5bfc578233
resampled <133955578+resampled@users.noreply.github.com>
2025-01-21 16:21:37 +1030

    +decouple_nk_to_name, hash_key -> hash_nk

--- webapp/namekeys.py
+++ webapp/namekeys.py
@@ -1,11 +1,16 @@
 from hashlib import sha256
 import base64
+import re
 
 def generate_nk_combo(name,key):
     if '#' in name:
         return '0'
     return f"{name}#{key}"
 
-def hash_key(key):
-    return base64.urlsafe_b64encode(bytes.fromhex(sha256(key.encode('utf-8')).hexdigest())).decode()
+def decouple_nk_to_name(inp):
+    name = re.search(r'.*(?=#)',inp).group()
+    return f"{name}"
+
+def hash_nk(nk):
+    return base64.urlsafe_b64encode(bytes.fromhex(sha256(nk.encode('utf-8')).hexdigest())).decode()
 
