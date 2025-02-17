0b7fd7dd6a0791321c62a129dc0458b934685b90
resampled <133955578+resampled@users.noreply.github.com>
2025-02-17 13:10:23 +1030

    xhr dynamic feed refreshing

--- webapp/static/room.js
+++ webapp/static/room.js
@@ -2,3 +2,10 @@
 // auto scroll to bottom of feed
 const feed=document.getElementById("r-feed");
 feed.scrollTop=feed.scrollHeight;
+// dynamic feed refreshing (5 sec delay)
+const delay = ms => new Promise(res => setTimeout(res, ms));
+const dyn = new XMLHttpRequest();
+dyn.onload = () => { rsp = dyn.responseXML.body.innerHTML; feed.innerHTML = rsp; }
+function refresh() { let xhrurl = '/=/xhr_room_feed?r='+'qqz'; dyn.open('GET',xhrurl); dyn.responseType = 'document'; dyn.send(); }
+i = 0;
+window.onload = async () => { while (true) { await delay(5000); i += 1; console.log('refresh #'+i); refresh(); } }
