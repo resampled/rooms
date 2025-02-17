6f490fc1141bb56c1a823208b598815c4be5b1e7
resampled <133955578+resampled@users.noreply.github.com>
2025-02-17 13:10:04 +1030

    const to feed declare

--- webapp/static/room.js
+++ webapp/static/room.js
@@ -1,3 +1,4 @@
 // this script runs only inside rooms & is deferred (runs AFTER fully loaded page)
-feed=document.getElementById("r-feed");
+// auto scroll to bottom of feed
+const feed=document.getElementById("r-feed");
 feed.scrollTop=feed.scrollHeight;
